5580f36ca12a3d77cf77f24a62de49c27cf71b58
Keith Puzey <keith.puzey@gmail.com>
2017-09-21 23:18:50 -0700

    Live and Virtual API Switch added to UI

--- medrec/physician/js/app.js
+++ medrec/physician/js/app.js
@@ -31,8 +31,13 @@ function DrugListCtrl($scope, $http) {
 		});
 
 	$scope.selectedItemChanged = function () {
+		$scope.hasDrugInfo = false;
+	}
+
+	$scope.liveButttonPressed = function () {
 		var brandname = $scope.selectedPrescription.NAME;
 		var URL = 'https://api.fda.gov/drug/event.json?search=brand_name:' + brandname;
+
 		console.log(URL);
 
 		$http.get(URL)
@@ -52,4 +57,95 @@ function DrugListCtrl($scope, $http) {
 				console.log("Response data is: " + data);
 			});
 	}
+
+	$scope.virtualButttonPressed = function () {
+		var brandname = $scope.selectedPrescription.NAME;
+		var URL = 'http://localhost:54084/drug/event.json?search=brand_name:' + brandname;
+
+		console.log(URL);
+
+		$http.get(URL)
+			.then(function (response) {
+				$scope.drugresult = response.data.results[0];
+				$scope.manufacturer = response.data.results[0].patient.drug[0].openfda.manufacturer_name;
+				$scope.hasDrugInfo = true;
+			}).
+			catch(function (response) {
+				$scope.hasDrugInfo = false;
+				// Handle error
+				var data = response.data;
+				var status = response.status;
+				var statusText = response.statusText;
+				var headers = response.headers;
+				var config = response.config;
+				console.log("Response data is: " + data);
+			});
+	}
+
+	$scope.toggle = true;
+
+	$scope.$watch('toggle', function () {
+		$scope.toggleText = $scope.toggle ? 'Live' : 'Virtual';
+	});
+
+	$scope.invokeButttonPressed = function () {
+		var URL;
+		var brandname = $scope.selectedPrescription.NAME;
+		if ($scope.toggleText == "Live") {
+			URL = 'https://api.fda.gov/drug/event.json?search=brand_name:' + brandname;
+		} else {
+			URL = 'http://localhost:54084/drug/event.json?search=brand_name:' + brandname;
+		}
+
+		console.log(URL);
+
+		$http.get(URL)
+			.then(function (response) {
+				$scope.drugresult = response.data.results[0];
+				$scope.manufacturer = response.data.results[0].patient.drug[0].openfda.manufacturer_name;
+				$scope.hasDrugInfo = true;
+			}).
+			catch(function (response) {
+				$scope.hasDrugInfo = false;
+				// Handle error
+				var data = response.data;
+				var status = response.status;
+				var statusText = response.statusText;
+				var headers = response.headers;
+				var config = response.config;
+				console.log("Response data is: " + data);
+			});
+	}
+
+
+
+
+	/*  Function to invoke API when the select drop-down changes
+	$scope.selectedItemChanged = function () {
+		var brandname = $scope.selectedPrescription.NAME;
+		var URL = 'https://api.fda.gov/drug/event.json?search=brand_name:' + brandname;     
+ 
+	/**	Desktop Virtual Service	     	
+		var URL = 'http://localhost:54084/drug/event.json?search=brand_name:' + brandname; 
+		console.log(URL);
+
+		$http.get(URL)
+			.then(function (response) {
+				$scope.drugresult = response.data.results[0];
+				$scope.manufacturer = response.data.results[0].patient.drug[0].openfda.manufacturer_name;
+				$scope.hasDrugInfo = true;
+			}).
+			catch(function (response) {
+				$scope.hasDrugInfo = false;
+				// Handle error
+				var data = response.data;
+				var status = response.status;
+				var statusText = response.statusText;
+				var headers = response.headers;
+				var config = response.config;
+				console.log("Response data is: " + data);
+			});
+	}
+	*/
+
 }
--- medrec/physician/js/patient.service.js
+++ medrec/physician/js/patient.service.js
@@ -1,26 +1,28 @@
 
 (function () {
 	var app = angular.module("medrecApp");
-    app.controller("PatientDetailsCtrl", PatientDetailsCtrl);
+	app.controller("PatientDetailsCtrl", PatientDetailsCtrl);
 
 
-    function PatientDetailsCtrl($scope, $http) {
+	function PatientDetailsCtrl($scope, $http) {
+
+		$scope.letterLimit = 10;
 
 		$http.get('http://172.17.17.91:8282/rest/abl/medrecdb/v1/main:PATIENTS', {
 			headers: { "Authorization": "Basic YWRtaW46UGFzc3dvcmQx" }
 		})
-		.then(function (response) {
-			$scope.patient = response.data;
-			console.log("Response is: " + $scope.patient);
-		}).
-		catch(function (response) {
-			// Handle error
-			var data = response.data;
-			var status = response.status;
-			var statusText = response.statusText;
-			var headers = response.headers;
-			var config = response.config;
-			console.log("Response data is: " + data);
-		});
-    }
+			.then(function (response) {
+				$scope.patient = response.data;
+				console.log("Response is: " + $scope.patient);
+			}).
+			catch(function (response) {
+				// Handle error
+				var data = response.data;
+				var status = response.status;
+				var statusText = response.statusText;
+				var headers = response.headers;
+				var config = response.config;
+				console.log("Response data is: " + data);
+			});
+	}
 })();
